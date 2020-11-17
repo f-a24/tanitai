3ebb54e7dbb205b495fe1cab523cfa8587e4a68a
Atsushi <niconyan713@gmail.com>
2020-11-18 02:22:59 +0900

    Updates

---
+++ precache-manifest.1da884ab64834569ee165d9f3752ff46.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "c95a24eb3a55f25c7b2a",
+    "url": "/tanitai/static/js/app.3aaededc.chunk.js"
+  },
+  {
+    "revision": "cc6b6a4effe0c8f1e477",
+    "url": "/tanitai/static/js/runtime~app.3504daa4.js"
+  },
+  {
+    "revision": "4c27103f21dd4788c624",
+    "url": "/tanitai/static/js/2.3c6c3ebe.chunk.js"
+  },
+  {
+    "revision": "e3fba8a9ea8a90d8c2d64a1f6544592b",
+    "url": "/tanitai/static/media/logo.95b40ad3.png"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/tanitai/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "49a79d66bdea2debf1832bf4d7aca127",
+    "url": "/tanitai/./fonts/SpaceMono-Regular.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/tanitai/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/tanitai/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/tanitai/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/tanitai/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/tanitai/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/tanitai/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/tanitai/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/tanitai/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/tanitai/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/tanitai/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/tanitai/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "ce5302dadd931f3cba06f444238b308f",
+    "url": "/tanitai/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/tanitai/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/tanitai/expo-service-worker.js"
+  },
+  {
+    "revision": "169a90eaa60dc7fb84254f02812836cd",
+    "url": "/tanitai/register-service-worker.js"
+  },
+  {
+    "revision": "8f174eb839fe0e7473e22e5a55e7ea23",
+    "url": "/tanitai/static/js/2.3c6c3ebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43a5130232c17bd79001a08eb99c39c5",
+    "url": "/tanitai/favicon-16.png"
+  },
+  {
+    "revision": "9930a88659c0a81d3b6db41306b1ca39",
+    "url": "/tanitai/favicon-32.png"
+  },
+  {
+    "revision": "fcb96b0ed7cd3ced85d2eb8d99011005",
+    "url": "/tanitai/favicon.ico"
+  },
+  {
+    "revision": "5a2c898d406f80afb687123cdef32b8f",
+    "url": "/tanitai/manifest.json"
+  }
+];
